018920e699de7f909251ffb1ae267c5402ecb375
rjolaverria <rafi.olaverria@thriveglobal.com>
2024-06-13 14:52:31 -0400

    init spring app

---
+++ src/main/kotlin/com/rjolaverria/attachmentsservice/AttachmentsServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.rjolaverria.attachmentsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AttachmentsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AttachmentsServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/rjolaverria/attachmentsservice/AttachmentsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rjolaverria.attachmentsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AttachmentsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
